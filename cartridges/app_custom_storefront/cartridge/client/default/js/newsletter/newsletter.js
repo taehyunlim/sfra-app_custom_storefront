d9d5faeb7b3f8f1d7930c7476a7c7527165390fd
taehyunlim <taehyun.lim@gmail.com>
2020-05-03 00:20:15 -0700

    form controller with client side js and csrf protection

---
+++ cartridges/app_custom_storefront/cartridge/client/default/js/newsletter/newsletter.js
@@ -0,0 +1,37 @@
+"use strict"
+
+var formValidation = require("base/components/formValidation")
+
+module.exports = {
+	newsletter: function () {
+		$("form.newsletter-form").submit(function (e) {
+			var $form = $(this)
+			e.preventDefault()
+			// url from pdict.actionUrl
+			var url = $form.attr("action")
+			$form.spinner().start()
+			$("form.newsletter-form").trigger("newsletter:submit", e)
+			$.ajax({
+				url: url,
+				type: "post",
+				dataType: "json",
+				data: $form.serialize(),
+				success: function (data) {
+					$form.spinner().stop
+					if (!data.success) {
+						formValidation($form, data)
+					} else {
+						window.location.href = data.redirectUrl
+					}
+				},
+				error: function (err) {
+					if (err.responseJSON.redirectUrl) {
+						window.location.href = err.responseJSON.redirectUrl
+					}
+					$form.spinner().stop()
+				}
+			})
+			return false
+		})
+	}
+}
--- cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
@@ -1,39 +1,57 @@
 "use strict"
 
 var server = require("server")
+var Resource = require("dw/web/Resource")
+var URLUtils = require("dw/web/URLUtils")
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf")
 
-server.get("Start", server.middleware.https, function (req, res, next) {
-	var actionUrl = dw.web.URLUtils.url("Newsletter-Handler")
-	var newsletterForm = server.forms.getForm("newsletter")
-	newsletterForm.clear()
+server.get(
+	"Start",
+	server.middleware.https,
+	csrfProtection.generateToken,
+	function (req, res, next) {
+		var actionUrl = URLUtils.url("Newsletter-Handler")
+		var newsletterForm = server.forms.getForm("newsletter")
+		newsletterForm.clear()
 
-	res.render("/newsletter/newslettersignup", {
-		actionUrl: actionUrl,
-		newsletterForm: newsletterForm
-	})
-
-	next()
-})
-
-server.post("Handler", server.middleware.https, function (req, res, next) {
-	var newsletterForm = server.forms.getForm("newsletter")
-	var continueUrl = dw.web.URLUtils.url("Newsletter-Start")
-
-	if (newsletterForm.valid) {
-		res.render("/newsletter/newslettersuccess", {
-			continueUrl: continueUrl,
+		res.render("/newsletter/newslettersignup", {
+			actionUrl: actionUrl,
 			newsletterForm: newsletterForm
 		})
-	} else {
-		res.render("/newsletter/newslettererror", {
-			errorMsg: dw.web.Resource.msg(
-				"error.crossfieldvalidation",
-				"newsletter",
-				null
-			),
-			continueUrl: continueUrl
-		})
+
+		next()
 	}
+)
+
+server.post(
+	"Handler",
+	csrfProtection.validateAjaxRequest,
+	server.middleware.https,
+	function (req, res, next) {
+		var newsletterForm = server.forms.getForm("newsletter")
+
+		if (newsletterForm.valid) {
+			res.json({
+				success: true,
+				redirectUrl: URLUtils.url("Newsletter-Success").toString()
+			})
+		} else {
+			res.setStatusCode(500)
+			res.json({
+				success: false,
+				error: [Resource.msg("error.crossfieldvalidation", "newsletter", null)]
+			})
+		}
+
+		next()
+	}
+)
+
+server.get("Success", server.middleware.https, function (req, res, next) {
+	res.render("/newsletter/newslettersuccess", {
+		continueUrl: URLUtils.url("Newsletter-Start"),
+		newsletterForm: server.forms.getForm("newsletter")
+	})
 
 	next()
 })
