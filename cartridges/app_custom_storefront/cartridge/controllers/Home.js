3fd61ed18b7d1c969fa2db9452744c53d3cc4de2
taehyunlim <taehyun.lim@gmail.com>
2020-05-07 20:29:01 -0700

    controller extend exercise

--- cartridges/app_custom_storefront/cartridge/controllers/Home.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Home.js
@@ -1,9 +1,15 @@
-var page = require("app_storefront_base/cartridge/controllers/Home")
+// var page = require("app_storefront_base/cartridge/controllers/Home")
 var server = require("server")
 
-server.extend(page)
+// server.extend(page)
+server.extend(module.superModule)
 
 server.append("Test", function (req, res, next) {
+	var viewdata = res.getViewData()
+	res.setViewData({
+		param1: "this is param1",
+		prevViewData: viewdata
+	})
 	res.json({ test: "Hello SFCC!" })
 	next()
 })
